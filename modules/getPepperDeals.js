6654385d40720a1b375a328b3a9dc0372e1ef90b
energywraith <energywraith@gmail.com>
2021-08-15 18:50:20 +0200

    Created getPepperDeals module
    getPepperDeals module is a function that returns deals from specified in params page (0 by default).
    - Installed axios for requesting the page,
    - Installed cheerio for parsing the HTML Document.

---
+++ modules/getPepperDeals.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const getPepperDeals = async (page = 0) => {
+  const firstPage = await axios.get(`https://www.pepper.pl/?page=${page}&ajax=true&layout=horizontal`);
+
+  if (!firstPage.data.data) return null;
+
+  let $ = cheerio.load(firstPage.data.data.content);
+  let articles = $('article.thread').not('.thread--expired');
+
+  articles = Array.from(articles).map(article => {
+    $ = cheerio.load(article);
+
+    return {
+      title: $('.thread-title').text(),
+      image: $('.thread-image').attr('src'),
+      price: $('.thread-price').text(),
+      articleLink: $('.thread-link').attr('href'),
+      dealLink: $('.cept-dealBtn').attr('href'),
+      votes: $('.vote-temp').text().replace(/(\r\n|\n|\r|\t)/gm, ""),
+      lifeTime: $('.metaRibbon .hide--toW3').text(),
+    };
+  })
+
+  return articles;
+}
+
+module.exports = getPepperDeals;
